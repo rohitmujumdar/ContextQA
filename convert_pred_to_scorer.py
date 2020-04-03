fddde8a3e4123d6d582da97b8b157ba000e05427
Shalini Chaudhuri <shalini.chaudhuri@gatech.edu>
2020-04-03 01:20:53 -0400

    making the file compatible with the scorer

---
+++ convert_pred_to_scorer.py
@@ -0,0 +1,42 @@
+import json
+
+
+def read_json():
+    c = {}
+    val = {}
+    with open('pred_out/pred1.json','r') as fp:
+        c = json.load(fp) 
+    with open('QuAC_data/dev.json','r') as fp:
+        val = json.load(fp) 
+    
+    return c, val
+
+def process_json():
+    c, val = read_json()
+
+    val_data = val['data']
+    predictions = c['predictions']
+
+    dict_answers = []
+
+    for i,p in enumerate(predictions):
+        corresponding_val_section = val_data[i]['paragraphs'][0]
+        questions = corresponding_val_section['qas']
+        
+        qids = list(map(lambda q: q['id'], questions))
+        yesno = list(map(lambda q: q['yesno'], questions))
+        followup = list(map(lambda q: q['followup'], questions))
+
+        obj = {}
+        obj['best_span_str'] = p
+        obj["qid"] = qids
+        obj["yesno"] = yesno
+        obj["followup"] = followup
+        dict_answers.append(obj)
+
+    
+    with open('pred_out/pred_eval_out.json','w') as fp:
+        json.dump(dict_answers, fp) 
+
+
+process_json()
